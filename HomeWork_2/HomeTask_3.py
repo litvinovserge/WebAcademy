ca97ea0b69be945a3c9e35917022c3e0095d9441
litvinovserge <litvinovserge@gmail.com>
2018-06-05 00:22:37 +0300

    HomeWork_2

--- HomeWork_2/HomeTask_3.py
+++ HomeWork_2/HomeTask_3.py
@@ -2,7 +2,7 @@
 Вывести таблицу умножения на экран.
 '''
 
-for i in range(2,10):
+for i in range(2, 10):
     print('Таблица умножения для', i)
     for ii in range(2, 10):
         print(i, '*', ii, '=', i * ii)
