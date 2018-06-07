f6b81658b3eadc5474dd6fd42a139cacec7f605c
litvinovserge <litvinovserge@gmail.com>
2018-06-07 15:44:18 +0300

    HomeWork_5

---
+++ HomeWork_3/HomeTask_5.py
@@ -0,0 +1,17 @@
+'''
+Написать функцию, которая выводит на экран n первых четных чисел.
+'''
+
+def even_finder(user_number):
+    user_number = int(user_number)
+    j = 0
+    for i in range(1, 3 * user_number):
+        if i % 2 == 0:
+            print(i)
+            j += 1
+        if j >= user_number:
+            break
+
+# начало основной программы
+n = input('Какое кол-во четных чисел вы хотите увидеть?')
+even_finder(n)
